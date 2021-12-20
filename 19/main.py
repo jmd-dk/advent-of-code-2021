af07a9f1eaa5c03e912dd3cab5add5f16babbf77
Jeppe Dakin <jeppe_dakin@hotmail.com>
2021-12-20 22:37:43 +0100

    finished solving day 19

--- 19/main.py
+++ 19/main.py
@@ -1,7 +1,7 @@
 import ast, collections, itertools
 import numpy as np
 
-# Part one (works on example but not custom input!)
+# Part one
 scanners = []
 with open('input.txt') as f:
     for line in f:
@@ -35,11 +35,13 @@ for rx in range(3):
                 if str(R) not in rotations_str:
                     rotations_str.add(str(R))
                     rotations.append(np.asarray(R))
-scanner_displacements = {0: (0, 0, 0)}
+scanner_displacements = {0: np.array((0, 0, 0))}
 scanner_orientations  = {0: rotations[0]}  # trivial rotation
 while len(scanner_displacements) < len(scanners):
     for i, scanner_i in enumerate(scanners):
+        i_bak = i
         for j, scanner_j in enumerate(scanners[i+1:], i + 1):
+            i = i_bak
             if (i in scanner_displacements) + (j in scanner_displacements) != 1:
                 continue
             for R_ij in rotations:
@@ -60,8 +62,7 @@ while len(scanner_displacements) < len(scanners):
                     # Relabel i <-> j so that i is the known scanner
                     # and j is the new scanner.
                     i, j = j, i
-                    R_ij, R_ji = R_ji, R_ij
-                    scanner_j = scanner_i
+                    R_ij = R_ji
                 R_i = scanner_orientations[i]
                 scanner_displacements[j] = R_i @ displacement + scanner_displacements[i]
                 scanner_orientations[j] = R_i @ R_ij
@@ -73,4 +74,11 @@ for i, scanner_i in enumerate(scanners):
         for beacon in scanner_i
     }
 print('part one:', len(beacons))
+
+# Part two
+max_dist = 0
+for displacement_i in scanner_displacements.values():
+    for displacement_j in scanner_displacements.values():
+        max_dist = max(max_dist, sum(np.abs(displacement_i - displacement_j)))
+print('part two:', max_dist)
 
